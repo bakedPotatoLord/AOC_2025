8355ba246e73b475aafa28498c03d99159689a49
bakedpotatolord <jeffy9745@gmail.com>
2025-12-01 21:48:29 -0700

    update template and helper func

--- scripts/helpers/index.ts
+++ scripts/helpers/index.ts
@@ -5,3 +5,5 @@ export {default as vec2} from "./vec2"
 export {default as matrix2} from "./matrix2"
 
 export {dispMatrix, getInput, numberSum, convertSign,v2, vectorEquals} from "./other"
+
+export  {abs, max, min, ceil, floor} from "./other"
--- scripts/template.ts
+++ scripts/template.ts
@@ -1,4 +1,4 @@
-import { dispMatrix, getInput, numberSum, convertSign, v2, vectorEquals, vec2 } from './helpers'
+import { dispMatrix, getInput, numberSum, convertSign, v2, vectorEquals, vec2, abs, max, min, ceil, floor } from './helpers'
 
 const raw = (await getInput(5))
   .split("\n")
