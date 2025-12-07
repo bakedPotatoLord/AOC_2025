bbcba7ed8767a8de8d9d3283f50b762f6ea339d0
bakedpotatolord <jeffy9745@gmail.com>
2025-12-07 13:51:26 -0700

    import text files, causing them to trigger auto-reloads

--- scripts/helpers/other.ts
+++ scripts/helpers/other.ts
@@ -8,7 +8,7 @@ export function numberSum(arr:number[]){
 
 export async function getInput(day:number){
   if(day === 0) throw new Error("No input for day 0")
-  return await fs.readFile(`inputs/${day}.txt`,{encoding:"utf8"})
+  return (await import(`../../inputs/${day}.txt`,{with: { type: "text/plain" }})).default
 }
 
 export function abs(n:number){
